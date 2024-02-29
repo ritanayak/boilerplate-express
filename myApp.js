d4c903cbf201331da9207245fb0ea395d6f78ba1
Rita Nayak <64993443+ritanayak@users.noreply.github.com>
2024-02-29 04:29:45 +0000

    serve asset files from the /public directory to the /public path

--- myApp.js
+++ myApp.js
@@ -6,8 +6,11 @@ let app = express();
 //app.get("/", (req, res) => {
     //res.send('Hello express');
 //})
+
+app.use("/public", express.static(__dirname + "/public"));
+
     app.get("/", (req, res) => {
-        res.sendFile(__dirname + "/views/index.html")
+        res.sendFile(__dirname + "/views/index.html");
     })
 
 
